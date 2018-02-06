a6bfa799c27283fadbec93641407601191a47047
Bryant Chow <bryant.h.c@gmail.com>
2018-02-06 14:47:34 +1300

    updating paper and figures

--- compare_obs_syn.py
+++ compare_obs_syn.py
@@ -4,9 +4,11 @@ import glob
 import obspy
 import numpy as np
 import matplotlib.pyplot as plt
-from obspy import read
+from obspy import read, read_events
+
 
 # paths and lists
+inv_path = './synthetics/specfem/input/tenGoodCMTSolutions'
 obs_list_path = './output/mseeds/'
 syn_list_path = './synthetics/specfem/output/mseeds/'
 
@@ -14,26 +16,34 @@ obs_list = glob.glob(obs_list_path + '*')
 syn_list = glob.glob(syn_list_path + '*RLAS.mseed')
 
 # unique tags for each event
-event_list = ['C201007181335A',
-            'C201109161926A',
-            'C201301050858A',
-            'C201304161044A',
-            'C201304191958A',
-            'C201502131859A',
-            'C201504250611A',
-            'C201509130814A',
-            'S201509162318A',
-            'C201601250422A']
+event_list = ['C201007181335A', #contains foreshock
+            'C201109161926A', #great, aftershock at end of trace
+            'C201301050858A', #great
+            'C201304161044A', #starts too early, foreshock?
+            'C201304191958A', #great
+            'C201502131859A', #starts too early
+            'C201504250611A', #long ringing, starts early
+            'C201509130814A', #good
+            'S201509162318A', #good
+            'C201601250422A'] #great
+
+# get event information
+event_info = read_events(inv_path)
+identifier = []
+for quakes in event_info:
+    id_temp = str(quakes.resource_id).split('/')[2]
+    index_temp = event_list.index(id_temp)
+    identifier.append(index_temp)
 
 # distances of each event in event_list to station RLAS
 ds_list = [76.3802716176,124.01982398,57.5432613254,89.721876944,18.1306704563,
         71.9959629677,109.912276463,27.994777703,80.2856479882,42.8349093379]
 mag_list = [6.63,7.92,6.42,6.1,7.36,7.56,7.14,7.11,6.7,7.77]
 
-pick = 'rr'
+pick = 'zv'
 
 # iterate on tags
-for i,event in enumerate(event_list):
+for i,(event,ident) in enumerate(zip(event_list,identifier)):
     # grab unique obs and syn by tag name
     for obs_tmp,syn_tmp in zip(obs_list,syn_list):
         if event in obs_tmp:
@@ -43,14 +53,19 @@ for i,event in enumerate(event_list):
     obs = read(obs_path,format='mseed')
     syn = read(syn_path,format='mseed')
 
+    # line up timing
+    quakeml = event_info[ident]
+    origin_time = quakeml.origins[0].time
+
     # filter values
     t_start = 10
-    t_end = 60
+    t_end = 30
     f_start = 1/t_end
     f_end = 1/t_start
 
     # preprocessing
     for streams in [obs,syn]:
+        streams.trim(starttime=origin_time,endtime=origin_time+2*3600)
         streams.detrend(type='linear')
         streams.taper(max_percentage=0.05)
         streams.filter('bandpass',freqmin=f_start,freqmax=f_end,corners=3,
@@ -60,9 +75,6 @@ for i,event in enumerate(event_list):
     obs_velocityZ = obs.select(id='GR.WET..BHZ')
     syn_velocityZ = syn.select(id='BW.RLAS.S3.MXZ').differentiate(
                                                             method='gradient')
-
-    syn_velocityZ.plot()
-    continue
 
     obs_rot_rateZ = obs.select(id='BW.RLAS..BJZ')
     syn_rot_rateZ = syn.select(id='BW.RLAS.S3.MYZ').differentiate(
@@ -86,6 +98,7 @@ for i,event in enumerate(event_list):
     # convert to time
     obs_SR = obs_rot_rateZ[0].stats.sampling_rate
     syn_SR = syn_rot_rateZ[0].stats.sampling_rate
+
     obs_maxtime = len(obs_data)/obs_SR
     syn_maxtime = len(syn_data)/syn_SR
     obs_time = np.linspace(0,obs_maxtime,len(obs_data))
@@ -103,52 +116,53 @@ for i,event in enumerate(event_list):
     print(tex_print)
 
     # plot
-    # plt.figure(1)
-    # ax1 = plt.subplot(111)
-    # a1= ax1.plot(obs_time,obs_data,'r',label='Obs.',zorder=2)
-    # ax1.plot(obs_peak_ind,obs_peak,'ro',zorder=3)
-    # ax1.annotate('Peak Obs. Amplitude: {amp:.2E} {units}'.format(
-    #                             amp=obs_peak,units=units),
-    #                             xy=(obs_peak_ind,obs_peak),zorder=4)
+    plt.figure(1)
+    ax1 = plt.subplot(111)
+    a1= ax1.plot(obs_time,obs_data,'r',label='Obs.',zorder=2)
+    ax1.plot(obs_peak_ind,obs_peak,'ro',zorder=3)
+    ax1.annotate('Peak Obs. Amplitude: {amp:.2E} {units}'.format(
+                                amp=obs_peak,units=units),
+                                xy=(obs_peak_ind,obs_peak),zorder=4)
 
-    # ax2 = ax1.twinx()
-    # a2 = ax2.plot(syn_time,syn_data,'k',label='Syn.',zorder=1)
-    # ax2.plot(syn_peak_ind,syn_peak,'ko',zorder=3)    
-    # ax2.annotate('Peak Syn. Amplitude: {amp:.2E} {units}'.format(
-    #                             amp=syn_peak,units=units),
-    #                             xy=(syn_peak_ind,syn_peak),zorder=4)
+    ax2 = ax1.twinx()
+    a2 = ax2.plot(syn_time,syn_data,'k',label='Syn.',zorder=1)
+    ax2.plot(syn_peak_ind,syn_peak,'ko',zorder=3)
+    ax2.annotate('Peak Syn. Amplitude: {amp:.2E} {units}'.format(
+                                amp=syn_peak,units=units),
+                                xy=(syn_peak_ind,syn_peak),zorder=4)
+    ax2.grid(None)
 
-    # ax1.set_xlabel('Time (s)')
-    # ax1.set_ylabel('Obs. Amplitude [{units}]'.format(units=units))
+
+    ax1.set_xlabel('Time (s)')
+    ax1.set_ylabel('Obs. Amplitude [{units}]'.format(units=units))
     # ax2.set_ylabel('Syn. Amplitude [{units}]'.format(units=units))
-    # ax1.grid()
-    
-    # ax1.set_title('{} / Event: {} / Event-Station Distance: {} deg'.format(choice,event,round(ds_list[i],2)))
-    # ax2.grid(None)    
+    ax1.grid()
 
-    # lines = a1 + a2
-    # labels = [l.get_label() for l in lines]
-    # ax1.legend(lines,labels,loc=0)
+    ax1.set_title('{} / Event: {} / Event-Station Distance: {} deg'.format(choice,event,round(ds_list[i],2)))
 
-    # figurename = os.path.join('./figures','waveforms',event+'.png')
-    # # plt.savefig(figurename,dpi=600,figsize=(11,7))
-    # plt.show()
+    lines = a1 + a2
+    labels = [l.get_label() for l in lines]
+    ax1.legend(lines,labels,loc=0)
 
-
+    figurename = os.path.join('./figures','waveforms',event+'.png')
+    # plt.savefig(figurename,dpi=600,figsize=(11,7))
+    plt.show()
 
 
     # compare zRR w/ tAC, keep in samples
-    # zRR_data = syn_rot_rateZ[0].data
-    # tAC_data = syn_accelT[0].data
+    zRR_data = syn_rot_rateZ[0].data
+    tAC_data = syn_accelT[0].data
 
-    # tAC_max = abs(max(tAC_data) and min(tAC_data))
-    # zRR_max = abs(max(zRR_data) and min(zRR_data))
+    tAC_max = abs(max(tAC_data) and min(tAC_data))
+    zRR_max = abs(max(zRR_data) and min(zRR_data))
 
-    # c = tAC_max/zRR_max
+    c = tAC_max/zRR_max
 
-    # tAC_data_scaled = [_/(2*c) for _ in tAC_data]
-    # print(c)
-    # # plot
+    tAC_data_scaled = [_/(2*c) for _ in tAC_data]
+    print(c)
+    # sys.exit()
+
+    # plot
     # plt.figure(1)
     # ax = plt.subplot(111)
     # a1 = ax1.plot(zRR_data,'r',label='RR.',zorder=1)
@@ -156,17 +170,8 @@ for i,event in enumerate(event_list):
     # ax1.set_xlabel('Samples')
     # ax1.set_ylabel('Amplitude')
     # ax1.grid()
-    
+
     # ax1.set_title(event)
     # plt.legend()
 
-    # plt.show()    
-
-
-
-
-
-
-
-
-
+    # plt.show()
---
+++ magscale.sh
@@ -0,0 +1,23 @@
+#python magscale.py --scale rr --sta specfem
+#python magscale.py --scale rt --sta specfem
+#python magscale.py --scale zv --sta specfem
+#python magscale.py --scale tv --sta specfem
+
+# python magscale.py --scale rr --sta wet
+# python magscale.py --scale rt --sta wet
+# python magscale.py --scale zv --sta wet
+# python magscale.py --scale tv --sta wet
+
+
+python magscale.py --scale zv --sta specfem --event 0
+python magscale.py --scale zv --sta specfem --event 1
+python magscale.py --scale zv --sta specfem --event 2
+python magscale.py --scale zv --sta specfem --event 3
+python magscale.py --scale zv --sta specfem --event 4
+python magscale.py --scale zv --sta specfem --event 5
+python magscale.py --scale zv --sta specfem --event 6
+python magscale.py --scale zv --sta specfem --event 7
+python magscale.py --scale zv --sta specfem --event 8
+python magscale.py --scale zv --sta specfem --event 9
+
+
